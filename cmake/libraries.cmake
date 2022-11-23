1e1dc17f12e490cca2c0b0a01284304f09d875ec
vaskozlov <vaskozlov3940@gmail.com>
2022-11-23 21:05:01 +0500

    Submodule package management system

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -16,4 +16,4 @@ find_package(Clang REQUIRED CONFIG)
 add_definitions(${CLANG_DEFINITIONS})
 include_directories(${CLANG_INCLUDE_DIRS})
 
-find_package(ccl REQUIRED)
+include_directories(ccl/include)
